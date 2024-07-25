2bed69fc93286227f1e70a5733185cd0b668fdd3
ouafcode <ouafaesaim@gmail.com>
2024-07-25 15:24:32 +0000

    task0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """ docs Class """
 
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ class child """
-    def __init__(self):
-        super().__init__()
 
     def put(self, key, item):
         """ put data """
-        if key is not None or item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """ get data """
