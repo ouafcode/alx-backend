61d19abb29bf297ccb0ada44a28002fa30cff294
ouafcode <ouafaesaim@gmail.com>
2024-08-04 10:24:33 +0000

    task4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ docs docs """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ docs docs """
+    if request.args.get("locale") in app.config["LANGUAGES"]:
+        return request.args.get("locale")
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ flask fucnt """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
